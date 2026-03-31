a0e407dae1c1cbc6232d770601ba2f49afdeab90
Dinesh <ceoa491@gmail.com>
2026-03-31 14:31:29 +0530

    Functions and Backend Integration Updates

---
+++ regh.js
@@ -0,0 +1,29 @@
+document.getElementById("reg").addEventListener("submit",function(e){
+    e.preventDefault();
+    let hname=document.getElementById("hname").value;
+    let name=document.getElementById("name").value;
+    let phone=document.getElementById("phone").value;
+    let email=document.getElementById("email").value;
+    let pass=document.getElementById("pass").value;
+    fetch("https://hotelmanagementbackend-production-5acc.up.railway.app/hreg",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({
+            hname:hname,
+            name:name,
+            phone:phone,
+            email:email,
+            pass:pass
+        })
+
+    })
+    .then(res=>{
+        res.text();
+    })
+    .then(data=>{
+        alert(data);
+    })
+    .catch(err=>{
+        console.error(err);
+    })
+})
