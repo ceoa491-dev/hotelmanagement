4728f431dde0b62411314f871a5dc923d718c648
Dinesh <ceoa491@gmail.com>
2026-03-31 19:22:33 +0530

    Customer Login Page Connected with Backend

---
+++ login2.js
@@ -0,0 +1,26 @@
+document.getElementById("log2").addEventListener("submit",function(e){
+    e.preventDefault();
+    let email=document.getElementById("email").value;
+    let pass=document.getElementById("pass").value;
+    fetch("https://hotelmanagementbackend-production-5acc.up.railway.app/clog",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({
+            email:email,
+            pass:pass
+        })
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        if(data.status==="success"){
+            alert("logged Successful");
+        }
+        else{
+            alert("loggin failed");
+        }
+    })
+    .catch(err=>{
+        console.error(err);
+        alert("error occured");
+    })
+})
