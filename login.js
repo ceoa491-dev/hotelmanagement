c5a6083ee7eac39a1587cda2ee390f7fe1cc230b
Dinesh <ceoa491@gmail.com>
2026-03-31 19:11:14 +0530

    Hotel Login Page Connected with Backend

---
+++ login.js
@@ -0,0 +1,26 @@
+document.getElementById("log").addEventListener("submit",function(e){
+    e.preventDefault();
+    let email=document.getElementById("email").value;
+    let pass=document.getElementById("pass").value;
+    fetch("https://hotelmanagementbackend-production-5acc.up.railway.app/hlog",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(({
+            email:email,
+            pass:pass
+        }))
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        if(data.status==="success"){
+            alert("logged successful");
+        }
+        else{
+            alert("failed to log");
+        }
+    })
+    .catch(err=>{console.error(err);
+        alert("Occured Error");
+    })
+
+})
