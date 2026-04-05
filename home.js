f725ebb70036abcaf7d131f1d52e9bc3d0f8518a
Dinesh <ceoa491@gmail.com>
2026-04-05 18:57:03 +0530

    Image and Button Functions In FrondEnd

--- home.js
+++ home.js
@@ -2,3 +2,65 @@ window.onload=function(){
 let hotelname=localStorage.getItem("hname");
 document.getElementById("namehotel").innerText=hotelname;
 }
+document.getElementById("addr").addEventListener("click",function(e){
+    e.preventDefault();
+    document.getElementById("subrr").addEventListener("submit",function(e){
+        e.preventDefault();
+        let txtno=document.getElementById("txtno").value;
+        let txtprice=document.getElementById("txtprice").value;
+        let files=document.getElementById("imgupp").files;
+        let formData=new FormData();
+        formData.append("txtno",txtno);
+        formData.append("txtprice",txtprice);
+        for(let i =0;i<=files.length;i++){
+            formData.append("images",files[i]); 
+        }
+        fetch("https://hotelmanagementbackend-production-a758.up.railway.app/addroom",{
+            method:"POST",
+            body:FormData
+        })
+        .then(res=>res.json())
+        .then(data=>{if(data.status=="success"){
+            alert("Room Added Successfully");
+
+        }
+    else{
+        alert ("Failed to Add Room");
+    }})
+
+        .then(err=>{
+            console.error(err);
+            alert("error occured");
+        })
+    })
+    document.getElementById("addr_form").style="display:flex";
+    document.getElementById("reset").addEventListener("click",function(e){
+        e.preventDefault();
+        document.getElementById("addr_form").style="display:none";
+        document.getElementById("txtno").value="";
+        document.getElementById("txtprice").value="";
+        document.getElementById("imgupp").value="";
+    })
+})
+document.getElementById("addf").addEventListener("click",function(e){
+    e.preventDefault();
+
+    document.getElementById("addf_form").style="display:flex";
+    document.getElementById("reset2").addEventListener("click",function(e){
+        e.preventDefault();
+        document.getElementById("addf_form").style="display:none";
+        document.getElementById("dis").value="";
+        document.getElementById("dispice").value="";
+        document.getElementById("imgupp2").value="";
+
+    })
+})
+document.getElementById("viewr").addEventListener("click",function(e){
+    e.preventDefault();
+    document.getElementById("lis").style="display:flex";
+})
+document.getElementById("viewf").addEventListener("click",function(e){
+    e.preventDefault();
+    document.getElementById("lisf").style="display:flex";
+})
+
