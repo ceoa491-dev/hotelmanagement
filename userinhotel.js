3afa51902b291c4bbe08e7e6795be7519d58653a
Dinesh <ceoa491@gmail.com>
2026-04-09 22:42:17 +0530

    err

--- userinhotel.js
+++ userinhotel.js
@@ -4,7 +4,8 @@ document.getElementById("room").addEventListener("click",function(e){
     fetch("https://hotelmanagementbackend-production-a758.up.railway.app/getroom")
     .then(res=>res.json())
     .then(data=>{
-        console.log(data.status);
+        console.log("inside data");
+        console.log(data);
         let container=document.getElementById("card");
         
         data.forEach(room=>{
