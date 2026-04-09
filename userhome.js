61249fb386adb3ff5eba935518f289c75262c3b5
Dinesh <ceoa491@gmail.com>
2026-04-09 19:46:06 +0530

    Hotel Information In Cards

--- userhome.js
+++ userhome.js
@@ -5,6 +5,17 @@ window.onload=function(){
     .then(res=>res.json())
     .then(data=>{
         console.log(data);
+        let card=document.getElementById("card");
+        card.innerHTML="";
+        data.forEach(all=>{
+            let carditems=document.createElement("div");
+            carditems.className="carditems";
+            let hnamel=document.createElement("lable");
+            hnamel.className="hname";
+            hnamel.innerText=all.hname;
+            carditems.appendChild(hnamel);
+            card.appendChild(carditems);
+        })
     })
     .catch(err=>{
         console.error(err);
