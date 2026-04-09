347674f76449a8ae3210cb202d936d8446678664
Dinesh <ceoa491@gmail.com>
2026-04-09 18:57:56 +0530

    Getting All Hotel Infromations From Backend

--- userhome.js
+++ userhome.js
@@ -1,4 +1,13 @@
 window.onload=function(){
     let username=localStorage.getItem("name");
     console.log(username);
+    fetch("https://hotelmanagementbackend-production-a758.up.railway.app/userhome")
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+    })
+    .catch(err=>{
+        console.error(err);
+        alert("error occured to load data");
+    })
 }
